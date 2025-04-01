2f971210c0f4bdb7ac10c09d06738327af784c1a
kaline gabrielly <gkaline769@gmail.com>
2025-04-01 15:48:59 -0300

    envio do exercicio 04

---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+a=int(input("Digite o primeiro numero:"))
+b=int(input("Digite o segundo numero:"))
+if a > b :
+    print(a,b)
+else:
+    print(b,a)
---
+++ teste01.py
@@ -0,0 +1,7 @@
+a= 10
+b= 3
+if a > b:
+    print("a é maior que b")
+else:
+    print("b é maior que a")
+
