1c86e4b01098d49d20b974aeab395e3b7877667c
kaline gabrielly <gkaline769@gmail.com>
2025-04-01 14:20:16 -0300

    atualização de codigo

--- exercicio01.py
+++ exercicio01.py
@@ -1,4 +1,6 @@
 nome= input("Digite seu nome:")
 idade= input("Digite sua idade:")
 salario= float(input("Digite o valor da sua renda mensal:"))
-print(f"O seu nome é {nome} , voce tem {idade} anos , e recebe {salario}")
+aumento= float(input("Digite o numero correspondente ao seu aumento:"))
+total= (salario*aumento)/100
+print(f"O seu nome é {nome} , voce tem {idade} anos , e recebia {salario} , com o aumento de {aumento} ira receber: {salario+total}")
