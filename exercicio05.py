4242ea3fd1e4565a49fbc8d41ee9f1b5ea0aab6d
kaline gabrielly <gkaline769@gmail.com>
2025-04-01 16:19:28 -0300

    alteracao de codigo

--- exercicio05.py
+++ exercicio05.py
@@ -2,7 +2,12 @@ nota1= float(input("Digite a primeira nota:"))
 nota2= float(input("Digite a segunda nota:"))
 nota3= float(input("Digitr a terceira nota:"))
 media= (nota1+nota2+nota3)/3
+
+
 if media >= 7:
     print(f"Aluno aprovado {media}")
 else:
-    print(f"Aluno reprovado {media}")
+    if media < 4:
+        print(f"Aluno recuperacao {media}")
+    else:
+        print(f"Aluno em recuperacao {media}")
