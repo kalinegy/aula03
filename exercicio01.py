81a3cb8e282186b0692056ff6598d7fd51739378
kaline gabrielly <gkaline769@gmail.com>
2025-04-01 14:01:51 -0300

    envio do primeiro exercicio

---
+++ exercicio01.py
@@ -0,0 +1,4 @@
+nome= input("Digite seu nome:")
+idade= input("Digite sua idade:")
+salario= float(input("Digite o valor da sua renda mensal:"))
+print(f"O seu nome é {nome} , voce tem {idade} anos , e recebe {salario}")
