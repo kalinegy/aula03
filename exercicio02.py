7757d3676c3fb661d002ad63d9f280b169cc3fac
kaline gabrielly <gkaline769@gmail.com>
2025-04-01 14:48:52 -0300

    novo arquivo

---
+++ exercicio02.py
@@ -0,0 +1,19 @@
+numero1= int(input("Digite o primeiro numero:"))
+numero2= int(input("Digite o segundo numero:"))
+resultadosoma= numero1+numero2
+print(f"A soma do numero {numero1} + {numero2} é igual a: {resultadosoma}")
+
+numero1= int(input("Digite o primeiro numero:"))
+numero2= int(input("Digite o segundo numero:"))
+resultadosub= numero1-numero2
+print(f"A subtracao do numero {numero1} - {numero2} é igual a: {resultadosub}")
+
+numero1= int(input("Digite o primeiro numero:"))
+numero2= int(input("Digite o segundo numero:"))
+resultadodiv= numero1/numero2
+print(f"A divisao do numero {numero1} / {numero2} é igual a: {resultadodiv}")
+
+numero1= int(input("Digite o primeiro numero:"))
+numero2= int(input("Digite o segundo numero:"))
+resultadomult= numero1*numero2
+print(f"A multiplicacao do numero {numero1} + {numero2} é igual a: {resultadomult}")
