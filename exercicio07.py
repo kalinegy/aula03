66e762525d9ea47ecf2afe13d40bcdeb505bf8e4
kaline gabrielly <gkaline769@gmail.com>
2025-04-03 14:29:08 -0300

    correção de código

--- exercicio07.py
+++ exercicio07.py
@@ -1,13 +1,17 @@
-
 Tipo= input("Digite qual tipo de combustivel\n"
-            "g para Gasolina \n"
+            "G para Gasolina \n"
             "E para Etanol")
 quantidade= float(input("Digite quantos litros você abasteceu:"))
 
 vGas= 5.8
 vEtal= 4.9
-if Tipo == "g":
+valor= 0
+if Tipo == "g" or Tipo == "G":
     valor= vGas*quantidade
 else:
-    valor= vEtal*quantidade
-print(f" Você gastou um total de R${valor}")
+    if Tipo == "e" or Tipo== "E":
+        valor= vEtal*quantidade
+    else:
+        print(f"Opção inválida")
+
+print(f"Você gastou R$ {valor}")
